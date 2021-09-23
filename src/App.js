64a43d80988e64ad1e8de4df34c0c2b241b0a15c
Muhdan Fyan Syah Sofian <muhdanfyan@gmail.com>
2021-09-23 21:30:15 +0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import Header from "./components/Header";
 
 function App() {
+  const title = "Welcome to My App"
+  const age = 20
+  const link = "https://mfikri.com"
+
   return (
-    <div className="App">
-      <h1>Welcome</h1>
+    <div>
+      <Header />
+      <h1>{title}</h1>
+      <h1>{ age * 2}</h1>
+      <a href={link}>Go To My Webiste</a>
     </div>
   );
 }
